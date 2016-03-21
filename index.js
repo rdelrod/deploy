6002238801cd0c5ae94a305b5eb7d499bbc7e9ec
Jared <jaredallard@outlook.com>
2016-03-21 13:56:20 -0700

    deploy: support pm2, replay events

--- index.js
+++ index.js
@@ -8,8 +8,10 @@
 
 'use strict';
 
-const express = require('express'),
+let express = require('express'),
+      request = require('request'),
       fs      = require('fs'),
+      replay  = require('request-replay'),
       async   = require('async'),
       nodegit = require('nodegit'),
       path    = require('path'),
@@ -24,7 +26,7 @@ const slog    = function() {
   log.apply(log, args);
 }
 const extend = (target, source) => {
-    for (var prop in source) {
+    for (let prop in source) {
       target[prop] = source[prop];
     }
     return target;
@@ -54,12 +56,11 @@ pm2.connect(function(err) {
     process.exit(2);
   }
 
-  log('deployments', 'pm2 started.');
+  log('deploy:pm2', 'daemon started.');
 
   config.deployments.forEach(function(v) {
     let spath = v.path;
     let sname = v.name;
-    let stype = v.type;
     let smain = v.main;
 
     let spm2   = v.pm2;
@@ -72,23 +73,24 @@ pm2.connect(function(err) {
 
     // check for pm2 override / additions
     if(spm2) {
-      if(spm2.opts) {
-        opts = extend(opts, spm2.opts);
+      opts = extend(opts, spm2.opts);
+    }
+
+    log('deploy:pm2', 'start', sname);
+    pm2.start(opts, function(err) {
+      if(err) {
+        sendEvent('error', sname, {
+          reason: 'Failed To Launch',
+          stage: 'init'
+        })
+        return log('deploy:pm2', 'app:', sname, 'failed to start with:', err);
       }
-    }
-
-    if(stype === 'nodejs') {
-      log('deployments', 'start', sname);
-      log('deployments', 'pm2 opts:', opts);
-
-      pm2.start(opts, function(err) {
-        if(err) {
-          return log('deployments', 'app:', sname, 'failed to start with:', err);
-        }
-      });
-    }
+    });
   });
 });
+
+let   STATUS             = 'idle',
+      RUNNINGDEPLOYMENTS = [];
 
 // Setup the REST API
 app.get('/', function(req, res) {
@@ -104,7 +106,10 @@ app.get('/', function(req, res) {
  * Get The Deployment status
  **/
 app.get('/status', function(req, res) {
-
+  return res.send({
+    status: STATUS,
+    running: RUNNINGDEPLOYMENTS
+  });
 });
 
 // Inject Socket.io for realtime updates.
@@ -115,11 +120,70 @@ let io = require('socket.io')(server);
 io.on('connection', function() {
   log('socket', 'recieved a connection')
 });
+
+const sendEvent = (event, repo, data) => {
+  if(event === 'status') {
+    if(data.inprogress) {
+      STATUS = 'running';
+    } else {
+      STATUS = 'idle';
+    }
+  }
+
+  // send the event
+  io.emit({
+    event: event,
+    repo: repo,
+    data: data
+  });
+
+  config.listeners.forEach(function(listener) {
+    log('event', 'send event', event, 'to', listener.uri);
+
+    replay(request(listener.uri, {
+      method: 'post',
+      body: {
+        event: 'deploy',
+        data: {
+          event: event,
+          repo: repo,
+          data: data
+        }
+      },
+      json: true
+    }, function (err, response, body) {
+
+    }), {
+      retries: 10,
+      factor: 3
+    })
+    .on('replay', function (replay) {
+      // "replay" is an object that contains some useful information
+      console.log('request failed: ' + replay.error.code + ' ' + replay.error.message);
+      console.log('replay nr: #' + replay.number);
+      console.log('will retry in: ' + replay.delay + 'ms')
+    });
+  });
+}
+
 server.listen(config.express.port);
 
 // trigger on push event.
-hook.on('push', function (repo, ref, data) {
-  log('githubhook', 'Got push event for:', repo);
+hook.on('push', function (repo, ref) {
+  log('githubhook', repo, 'push event');
+
+  sendEvent('deploy', 'recieved deploy request for: '+repo)
+
+  sendEvent('status', repo, {
+    inprogress: true
+  });
+
+  // push the code to our running table.
+  RUNNINGDEPLOYMENTS.push({
+    name: repo,
+    status: 'running',
+    started: Date.now()
+  });
 
   let deployConfig = false;
   config.deployments.forEach((v) => {
@@ -135,6 +199,17 @@ hook.on('push', function (repo, ref, data) {
   // get our forever context
   async.waterfall([
     /**
+     * Verify we're on the right branch.
+     **/
+    function(next) {
+      if(ref !== config.githubhook.branch) {
+        return next('Not Production branch: '+ref);
+      }
+
+      return next();
+    },
+
+    /**
      * Get the Forever Script context
      **/
     function(next) {
@@ -143,18 +218,32 @@ hook.on('push', function (repo, ref, data) {
           return log('github:pm2', 'Failed to obtain list of process.');
         }
 
-        log('github:pm2', list);
+        let proc = false;
+        list.forEach((p) => {
+          if(p.name === repo) {
+            proc = p;
+          }
+        });
+
+        if(!proc) {
+          return next('We dont support: '+repo);
+        }
+
+        sendEvent('deploy', repo, 'found pm2 process');
+
+        return next(false, proc);
       })
     },
 
     /**
      * Git "Pull"
      **/
-    function(foreverContext, next) {
+    function(proc, next) {
       let spath = deployConfig.path;
       let repository;
 
-      log('githubhook', 'pulling new code...');
+      log('github:git', 'pulling new code...');
+      sendEvent('deploy', repo, 'pulling origin & merging.')
 
       nodegit.Repository.open(spath)
         .then(function(repo) {
@@ -174,26 +263,62 @@ hook.on('push', function (repo, ref, data) {
         // Now that we're finished fetching, go ahead and merge our local branch
         // with the new one
         .then(function() {
-          return repository.mergeBranches('master', 'origin/master');
+          return repository.mergeBranches('production', 'origin/production');
         })
         .done(function() {
-          return next(false, foreverContext);
+          sendEvent('deploy', repo, 'finished pulling production branch.');
+          return next(false, proc);
         });
     },
 
     /**
      * Restart the Script
      **/
-     function(foreverContext, next) {
-       log('githubhook', 'restart service:', repo);
-       return next();
+     function(proc, next) {
+       log('github:pm2', 'restart service:', repo);
+       sendEvent('deploy', repo, 'restart pm2 process.');
+       pm2.restart(repo, (err) => {
+         if(err) {
+           return next(err);
+         }
+
+         log('github:pm2', 'restarted:', repo, 'successfully');
+         sendEvent('deploy', repo, 'pm2 process restart');
+         return next();
+       })
      }
   ], function(err) {
+
+    // Remove the deployment regardless of status
+    let index = false;
+    RUNNINGDEPLOYMENTS.forEach((v, i) => {
+      if(v.name === repo) {
+        index = i;
+      }
+    })
+
+    log('github:express', 'Found RUNNINGDEPLOYMENTS#'+index, 'for pm2', repo);
+    RUNNINGDEPLOYMENTS.splice(index, 1);
+
     if(err) {
+      sendEvent('deploy', repo, 'deploy failed.');
+      sendEvent('status', repo, {
+        inprogress: false,
+        success: false
+      });
+      sendEvent('error', repo, {
+        reason: err,
+        stage: 'deploy'
+      });
       return log('githubhook', 'error:', err);
     }
 
     log('githubhook', 'deployed successfully.');
+    sendEvent('deploy', repo, 'deploy finised.');
+    sendEvent('status', repo, {
+      inprogress: false,
+      success: true
+    });
   });
 });
 
@@ -203,13 +328,18 @@ hook.listen();
 slog('initialized')
 
 process.on('SIGINT', function() {
-  slog( "Gracefully shutting down from SIGINT (Ctrl-C)" );
+  console.log();
+  slog('CTRL-C');
   process.exit();
 });
 
 process.on('exit', function(code) {
   slog('EXITING: THIS IS BAD.');
 
+  pm2.list((p) => {
+    slog('pm2: stop', p.name);
+    pm2.stop(p.name);
+  });
   // do logic to determine if crash or etc.
   slog('exit code:', code);
 });
